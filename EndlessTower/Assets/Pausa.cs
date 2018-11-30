a45a9792569882e08217d59710b8e209b2c4c8a4
leinegar <leinegar@gmail.com>
2018-11-30 20:56:20 -0300

    Malo
    cambio malo

---
+++ EndlessTower/Assets/Pausa.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+
+public class Pausa : MonoBehaviour
+{
+    Canvas canvas;
+
+	// Use this for initialization
+	void Start ()
+    {
+        canvas = GetComponent<Canvas>();
+        canvas.enabled= false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Pause();
+        }
+	}
+
+    public void Pause()
+    {
+        canvas.enabled = !canvas.enabled;
+        Time.timeScale = Time.timeScale == 0 ? 1 : 0;
+    }
+
+    public void Quit()
+    {
+        #if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+        #else
+        Application.Quit();
+        #endif
+    }
+
+}
